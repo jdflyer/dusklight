92f29bc5f977e0296a1a4644be0c547963f14d15
jdflyer <jdflyer10@gmail.com>
2026-07-06 14:57:28 -0700

    Fix map warping on mirror mode

--- src/d/d_menu_fmap.cpp
+++ src/d/d_menu_fmap.cpp
@@ -2486,11 +2486,6 @@ void dMenu_Fmap_c::portalWarpMapMove(STControl* i_stick) {
     f32 arrow_y = mpDraw2DBack->getArrowPos2DY();
     u8 uVar6 = 0xff;
 
-#if TARGET_PC
-    if (dusk::getSettings().game.enableMirrorMode) {
-        arrow_x = mpDraw2DBack->getMirrorPosX(arrow_x, 0.0f);
-    }
-#endif
 
 
     for (int i = 0; i < portal_dat->mCount; i++) {
